a4465c8fb175b5f847a24ccf3d0c4d1868688ce8
Aleksey Rodionov <rodion_obn@mail.ru>
2023-10-02 23:45:45 +0300

    Task1 and 2 completed

--- Seminar1/Task1/src/main/java/org/example/Main.java
+++ Seminar1/Task1/src/main/java/org/example/Main.java
@@ -4,7 +4,21 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        /**
+         * 1. Создать класс Cat с полями:
+         * имя, цвет, количество лет. Поработать с полями напрямую
+         */
+
+        Cat cat1 = new Cat("Барсик", "Рыжий", 7);
+        cat1.setAge(-10);
+        cat1.setAge(10);
+
+
+        /**
+         * 2. Добавить инкапсуляцию и сделать валидацию на введенный возраст
+         */
+
+         System.out.println("имя: " + cat1.getName() + " цвет: " + cat1.getColor() + " возраст:" + cat1.getAge());
 
 
     }
